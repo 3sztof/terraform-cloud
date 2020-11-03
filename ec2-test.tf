7f0e5a67d3e394d16f33f7b617fba302de5b05cf
Krzysztof Wilczynski <krzywil@amazon.com>
2020-11-03 16:31:26 +0100

    Added a gitignore and updated the template to use vars with credentials.

--- ec2-test.tf
+++ ec2-test.tf
@@ -1,8 +1,8 @@
 # Create a new instance of the latest Ubuntu 20.04 on an
 # t3.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
-  #access_key = var.access_key
-  #secret_key = var.secret_key
+  access_key = var.access_key
+  secret_key = var.secret_key
   region = "us-east-1"
 }
 
